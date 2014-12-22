035d3f5b7f4b72f28baf97c8aec60e0b39dabec8
enrico <enrico.comiti@gmail.com>
2014-12-22 13:42:04 +0100

    fmt cleanup

--- util_windows.go
+++ util_windows.go
@@ -14,13 +14,13 @@ func isatty(fd uintptr) bool {
 // Work around to Win os.Rename implementation:
 // http://code.google.com/p/go/issues/detail?id=3366
 func renameFile(oldpath, newpath string) error {
-    b, err := ioutil.ReadFile(oldpath)
-    if err != nil {
-        return err
-    }
-    err = ioutil.WriteFile(newpath, b, 0644)
-    if err != nil {
-        return err
-    }
-    return os.Remove(oldpath)
+	b, err := ioutil.ReadFile(oldpath)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(newpath, b, 0644)
+	if err != nil {
+		return err
+	}
+	return os.Remove(oldpath)
 }
